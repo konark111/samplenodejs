284b777fd8df641029527089c49697da79274431
konark111 <136243900+konark111@users.noreply.github.com>
2023-06-15 12:25:11 +0530

    Update2 index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Testing DevOps Node! JS Project Hello How are you ? i am guggs and this is my friend haddi')
+  response.send('Hello Testing DevOps Node! JS Project Hello How are you ? i am guggs and this is my friend haddi and babua')
 }) 
 
 app.listen(app.get('port'), function() {
